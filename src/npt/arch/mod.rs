17378df30b3d3934cd67b424f418d11d0c206a68
hky1999 <976929993@qq.com>
2024-08-10 12:36:17 +0800

    Update page_table_multiarch to v0.3.3, remove redudant file for riscv npt

--- src/npt/arch/mod.rs
+++ src/npt/arch/mod.rs
@@ -4,11 +4,9 @@ cfg_if::cfg_if! {
     if #[cfg(target_arch = "x86_64")] {
         mod x86_64;
         pub use self::x86_64::*;
-    } else if #[cfg(target_arch = "riscv64")] {
-        mod riscv64;
-        pub use self::riscv64::*;
     } else if #[cfg(target_arch = "aarch64")] {
         mod aarch64;
         pub use self::aarch64::*;
     }
+    // The Nested Page Table in the RISC-V architecture directly reuses ArceOS's page table.
 }
